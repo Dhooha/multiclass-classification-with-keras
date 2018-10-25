e6948c108b144deefe598a82ec63cfc0bf5c60c0
Dhoha Abid <abid.dhoha@gmail.com>
2018-10-25 16:55:02 -0400

    code for the train a multi-class network

---
+++ train_test_multiclass_net.py
@@ -0,0 +1,48 @@
+from sklearn.datasets import load_iris
+import numpy as np
+
+
+def train_test():
+
+    # load the data
+    iris = load_iris()
+    X = iris.data[:, 0:4]  # X for the set of features
+    y = iris.target  # for the set of labels
+    print(np.unique(y))  # to print the labels
+
+    # One Hot Encode Y: necessary for classification. It seems that this step encode the labels (1, 2, 3)
+    # into binary coding
+    from sklearn.preprocessing import LabelBinarizer
+    encoder = LabelBinarizer()
+    Y = encoder.fit_transform(y)
+
+    neural_network = create_network()
+    neural_network.fit(X, Y, epochs=500, batch_size=10)
+
+
+def create_network():
+    """
+    This function defines the structure of the neural network
+    :return:
+    """
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.optimizers import SGD
+
+    model = Sequential()
+    model.add(Dense(5, input_shape=(4, ), activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+
+    # stochastic gradient descent
+    sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+    return model
+
+
+def main():
+    train_test()
+
+
+if __name__ == '__main__':
+    main()
